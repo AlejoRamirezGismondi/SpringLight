e67c689ab4d214128a5504faeb65dee98f6df74b
Alejo Ramirez Gismondi <alejo.ramirezgismondi@ing.austral.edu.ar>
2022-11-05 20:10:29 -0300

    Working on script to generate sprite asset lib

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,16 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private int sceneBuildIndex;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        SpriteLibraryGenerator.GenerateSpriteLibrary("Test");
+    }
 
     public void ResetGame()
     {
---
+++ Assets/Scripts/SpriteLibraryGenerator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.U2D.Animation;
+
+public static class SpriteLibraryGenerator
+{
+    public static void GenerateSpriteLibrary(string name)
+    {
+        // TODO save images to folder
+
+        var spriteLib = ScriptableObject.CreateInstance<SpriteLibraryAsset>();
+        
+        // TODO generate methods to add all categories
+        // TODO get sprites from folder
+        
+        var sprite = Resources.Load<Sprite>(name + "/" + name);
+
+        spriteLib.AddCategoryLabel(null, "Idle_Down", "Entry");
+        
+        AssetDatabase.CreateAsset(spriteLib, "Assets/Artwork/Character/Sprite_Libraries/" + name + ".asset");
+    }
+}
