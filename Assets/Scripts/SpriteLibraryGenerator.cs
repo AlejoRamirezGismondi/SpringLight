e5bbddcb678c69a5f79b60446e78b1bac72ab7ca
Alejo Ramirez Gismondi <alejo.ramirezgismondi@ing.austral.edu.ar>
2022-11-06 16:57:17 -0300

    Can create sprite library from code

--- Assets/Scripts/SpriteLibraryGenerator.cs
+++ Assets/Scripts/SpriteLibraryGenerator.cs
@@ -1,4 +1,6 @@
-﻿using UnityEditor;
+﻿using Unity.VisualScripting;
+using UnityEditor;
+using UnityEditor.U2D.Animation;
 using UnityEngine;
 using UnityEngine.U2D.Animation;
 
@@ -6,17 +8,36 @@ public static class SpriteLibraryGenerator
 {
     public static void GenerateSpriteLibrary(string name)
     {
-        // TODO save images to folder
-
         var spriteLib = ScriptableObject.CreateInstance<SpriteLibraryAsset>();
         
         // TODO generate methods to add all categories
         // TODO get sprites from folder
+        // var sprites = ResourcesFolderLoader.LoadAllSpritesInDefault();
         
         var sprite = Resources.Load<Sprite>(name + "/" + name);
 
-        spriteLib.AddCategoryLabel(null, "Idle_Down", "Entry");
-        
+        spriteLib.AddCategoryLabel(sprite, "Idle_Down", "0");
+        spriteLib.AddCategoryLabel(sprite, "Idle_Up", "4");
+        spriteLib.AddCategoryLabel(sprite, "Idle_Right", "8");
+        spriteLib.AddCategoryLabel(sprite, "Idle_Left", "12");
+        spriteLib.AddCategoryLabel(sprite, "Walk_Down", "0");
+        spriteLib.AddCategoryLabel(sprite, "Walk_Down", "1");
+        spriteLib.AddCategoryLabel(sprite, "Walk_Down", "2");
+        spriteLib.AddCategoryLabel(sprite, "Walk_Down", "3");
+        spriteLib.AddCategoryLabel(sprite, "Walk_Up", "4");
+        spriteLib.AddCategoryLabel(sprite, "Walk_Up", "5");
+        spriteLib.AddCategoryLabel(sprite, "Walk_Up", "6");
+        spriteLib.AddCategoryLabel(sprite, "Walk_Up", "7");
+        spriteLib.AddCategoryLabel(sprite, "Walk_Right", "8");
+        spriteLib.AddCategoryLabel(sprite, "Walk_Right", "9");
+        spriteLib.AddCategoryLabel(sprite, "Walk_Right", "10");
+        spriteLib.AddCategoryLabel(sprite, "Walk_Right", "11");
+        spriteLib.AddCategoryLabel(sprite, "Walk_Left", "12");
+        spriteLib.AddCategoryLabel(sprite, "Walk_Left", "13");
+        spriteLib.AddCategoryLabel(sprite, "Walk_Left", "14");
+        spriteLib.AddCategoryLabel(sprite, "Walk_Left", "15");
+
         AssetDatabase.CreateAsset(spriteLib, "Assets/Artwork/Character/Sprite_Libraries/" + name + ".asset");
+        AssetDatabase.SaveAssets();
     }
 }
