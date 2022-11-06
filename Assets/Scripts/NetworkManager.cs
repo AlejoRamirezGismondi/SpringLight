a0889c01a3b32a2f58416d7f20df99fc9bc9af8f
Alejo Ramirez Gismondi <alejo.ramirezgismondi@ing.austral.edu.ar>
2022-11-06 19:46:09 -0300

    GET request Menu UI

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkManager : MonoBehaviour
+{
+
+    [SerializeField] private string uri = "http://localhost:8080/sprites/2";
+    
+    private void Start() {
+        // StartCoroutine(MakeRequests());
+    }
+
+    private IEnumerator MakeRequests() {
+        // GET
+        var getRequest = CreateRequest(uri);
+        yield return getRequest.SendWebRequest();
+        var deserializedGetData = JsonUtility.FromJson<SpriteDto>(getRequest.downloadHandler.text);
+        
+        AssetDatabase.CreateFolder("Assets/Artwork/Character/Resources", deserializedGetData.name);
+
+        var n = 0;
+        for (int i = 0; i < deserializedGetData.front.Length; i++)
+        {
+            var imageRequest = CreateRequest(uri);
+            yield return imageRequest.SendWebRequest();
+            var image = JsonUtility.FromJson<string>(getRequest.downloadHandler.text);
+            SaveImage(image, deserializedGetData.name, n);
+            n++;
+        }
+        
+        for (int i = 0; i < deserializedGetData.back.Length; i++)
+        {
+            var imageRequest = CreateRequest(uri);
+            yield return imageRequest.SendWebRequest();
+            var image = JsonUtility.FromJson<string>(getRequest.downloadHandler.text);
+            SaveImage(image, deserializedGetData.name, n);
+            n++;
+        }
+        
+        for (int i = 0; i < deserializedGetData.right.Length; i++)
+        {
+            var imageRequest = CreateRequest(uri);
+            yield return imageRequest.SendWebRequest();
+            var image = JsonUtility.FromJson<string>(getRequest.downloadHandler.text);
+            SaveImage(image, deserializedGetData.name, n);
+            n++;
+        }
+
+        // Trigger continuation of game flow
+    }
+
+
+    private static UnityWebRequest CreateRequest(string path, RequestType type = RequestType.GET, object data = null) {
+        var request = new UnityWebRequest(path, type.ToString());
+
+        if (data != null) {
+            var bodyRaw = Encoding.UTF8.GetBytes(JsonUtility.ToJson(data));
+            request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        }
+
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        return request;
+    }
+    
+    private static void SaveImage(string base64, string name, int n)
+    {
+        var fileNameResources = $"Assets/Artwork/Character/Resources/{name}/{name}_{n}.asset";
+
+        byte[]  imageBytes = Convert.FromBase64String(base64);
+        Texture2D tex = new Texture2D(2, 2);
+        tex.LoadImage( imageBytes );
+        Sprite sprite = Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0.5f, 0.5f), 100.0f);
+        
+        // Save the sprite (doesn't work on build)
+        AssetDatabase.CreateAsset(sprite, fileNameResources);
+    }
+}
+
+public enum RequestType {
+    GET = 0,
+    POST = 1,
+    PUT = 2
+}
+
+public class SpriteDto {
+    // Ensure no getters / setters
+    // Typecase has to match exactly
+    public int id;
+    public string name;
+    public string[] front;
+    public string[] back;
+    public string[] right;
+}
--- Assets/Scripts/SpriteLibraryGenerator.cs
+++ Assets/Scripts/SpriteLibraryGenerator.cs
@@ -8,36 +8,36 @@ public static class SpriteLibraryGenerator
 {
     public static void GenerateSpriteLibrary(string name)
     {
-        var spriteLib = ScriptableObject.CreateInstance<SpriteLibraryAsset>();
-        
-        // TODO generate methods to add all categories
-        // TODO get sprites from folder
-        // var sprites = ResourcesFolderLoader.LoadAllSpritesInDefault();
-        
-        var sprite = Resources.Load<Sprite>(name + "/" + name);
-
-        spriteLib.AddCategoryLabel(sprite, "Idle_Down", "0");
-        spriteLib.AddCategoryLabel(sprite, "Idle_Up", "4");
-        spriteLib.AddCategoryLabel(sprite, "Idle_Right", "8");
-        spriteLib.AddCategoryLabel(sprite, "Idle_Left", "12");
-        spriteLib.AddCategoryLabel(sprite, "Walk_Down", "0");
-        spriteLib.AddCategoryLabel(sprite, "Walk_Down", "1");
-        spriteLib.AddCategoryLabel(sprite, "Walk_Down", "2");
-        spriteLib.AddCategoryLabel(sprite, "Walk_Down", "3");
-        spriteLib.AddCategoryLabel(sprite, "Walk_Up", "4");
-        spriteLib.AddCategoryLabel(sprite, "Walk_Up", "5");
-        spriteLib.AddCategoryLabel(sprite, "Walk_Up", "6");
-        spriteLib.AddCategoryLabel(sprite, "Walk_Up", "7");
-        spriteLib.AddCategoryLabel(sprite, "Walk_Right", "8");
-        spriteLib.AddCategoryLabel(sprite, "Walk_Right", "9");
-        spriteLib.AddCategoryLabel(sprite, "Walk_Right", "10");
-        spriteLib.AddCategoryLabel(sprite, "Walk_Right", "11");
-        spriteLib.AddCategoryLabel(sprite, "Walk_Left", "12");
-        spriteLib.AddCategoryLabel(sprite, "Walk_Left", "13");
-        spriteLib.AddCategoryLabel(sprite, "Walk_Left", "14");
-        spriteLib.AddCategoryLabel(sprite, "Walk_Left", "15");
-
-        AssetDatabase.CreateAsset(spriteLib, "Assets/Artwork/Character/Sprite_Libraries/" + name + ".asset");
-        AssetDatabase.SaveAssets();
+        // var spriteLib = ScriptableObject.CreateInstance<SpriteLibraryAsset>();
+        //
+        // // TODO generate methods to add all categories
+        // // TODO get sprites from folder
+        // // var sprites = ResourcesFolderLoader.LoadAllSpritesInDefault();
+        //
+        // var sprite = Resources.Load<Sprite>(name + "/" + name);
+        //
+        // spriteLib.AddCategoryLabel(sprite, "Idle_Down", "0");
+        // spriteLib.AddCategoryLabel(sprite, "Idle_Up", "4");
+        // spriteLib.AddCategoryLabel(sprite, "Idle_Right", "8");
+        // spriteLib.AddCategoryLabel(sprite, "Idle_Left", "12");
+        // spriteLib.AddCategoryLabel(sprite, "Walk_Down", "0");
+        // spriteLib.AddCategoryLabel(sprite, "Walk_Down", "1");
+        // spriteLib.AddCategoryLabel(sprite, "Walk_Down", "2");
+        // spriteLib.AddCategoryLabel(sprite, "Walk_Down", "3");
+        // spriteLib.AddCategoryLabel(sprite, "Walk_Up", "4");
+        // spriteLib.AddCategoryLabel(sprite, "Walk_Up", "5");
+        // spriteLib.AddCategoryLabel(sprite, "Walk_Up", "6");
+        // spriteLib.AddCategoryLabel(sprite, "Walk_Up", "7");
+        // spriteLib.AddCategoryLabel(sprite, "Walk_Right", "8");
+        // spriteLib.AddCategoryLabel(sprite, "Walk_Right", "9");
+        // spriteLib.AddCategoryLabel(sprite, "Walk_Right", "10");
+        // spriteLib.AddCategoryLabel(sprite, "Walk_Right", "11");
+        // spriteLib.AddCategoryLabel(sprite, "Walk_Left", "12");
+        // spriteLib.AddCategoryLabel(sprite, "Walk_Left", "13");
+        // spriteLib.AddCategoryLabel(sprite, "Walk_Left", "14");
+        // spriteLib.AddCategoryLabel(sprite, "Walk_Left", "15");
+        //
+        // AssetDatabase.CreateAsset(spriteLib, "Assets/Artwork/Character/Sprite_Libraries/" + name + ".asset");
+        // AssetDatabase.SaveAssets();
     }
 }
