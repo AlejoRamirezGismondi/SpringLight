1766691c09c77c46070938b43eebd41f45afc6ec
Alejo Ramirez Gismondi <alejo.ramirezgismondi@ing.austral.edu.ar>
2022-12-11 23:24:18 -0300

    (chore): Crop and CropState generated with ChatGPT

---
+++ Assets/Scripts/Crops/CropState/CropState.cs
@@ -0,0 +1,48 @@
+﻿namespace Crops.CropState
+{
+    public abstract class CropState
+    {
+        public abstract CropState NextState();
+    }
+    
+    public class UnplowedState : CropState
+    {
+        public override CropState NextState()
+        {
+            return new PlowedState();
+        }
+    }
+    
+    public class PlowedState : CropState
+    {
+        public override CropState NextState()
+        {
+            return new SeededState();
+        }
+    }
+
+    public class SeededState : CropState
+    {
+        public override CropState NextState()
+        {
+            return new GrowingState();
+        }
+    }
+
+    public class GrowingState : CropState
+    {
+        public override CropState NextState()
+        {
+            return new GrownState();
+        }
+    }
+
+    public class GrownState : CropState
+    {
+        public override CropState NextState()
+        {
+            // Do something when the crop is fully grown
+            return new UnplowedState();
+        }
+    }
+}
---
+++ Assets/Scripts/Crops/CropTile.cs
@@ -0,0 +1,19 @@
+﻿using Crops.CropState;
+
+namespace Crops
+{
+    public class CropTile
+    {
+        private CropState.CropState State { get; set; }
+
+        public CropTile()
+        {
+            State = new UnplowedState();
+        }
+
+        public void NextState()
+        {
+            State = State.NextState();
+        }
+    }
+}
