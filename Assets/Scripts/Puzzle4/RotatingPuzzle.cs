d70d461dcb24f80c0aa06a258d1725fb86ecb27d
Alejo Ramirez Gismondi <alejo.ramirezgismondi@ing.austral.edu.ar>
2023-01-16 02:12:39 -0300

    (Chore): Finished Puzzle4. Missing reward

---
+++ Assets/Scripts/Puzzle4/RotatingPuzzle.cs
@@ -0,0 +1,36 @@
+using Inventory.Scripts;
+
+namespace Puzzle4
+{
+    public class RotatingPuzzle : Interactable
+    {
+        private RotatingPuzzleManager _manager;
+
+        private void Awake()
+        {
+            _manager = GetComponentInParent<RotatingPuzzleManager>();
+        }
+
+        public override void Interact(InventoryComponent inventoryComponent)
+        {
+            Rotate();
+            _manager.PuzzleWasRotated();
+        }
+
+        private void Rotate()
+        {
+            // Rotate transform 90 degrees clockwise
+            transform.Rotate(0, 0, -90);
+        }
+
+        public void Rotate(int times)
+        {
+            for (int i = 0; i < times; i++) Rotate();
+        }
+
+        public bool IsUpright()
+        {
+            return transform.rotation.z % 360 == 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Puzzle4/RotatingPuzzleManager.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Puzzle4
+{
+    public class RotatingPuzzleManager : MonoBehaviour
+    {
+        private RotatingPuzzle[] _puzzles;
+
+        private void Awake()
+        {
+            _puzzles = GetComponentsInChildren<RotatingPuzzle>();
+            // Sort puzzles by name
+            Array.Sort(_puzzles, (a, b) => String.Compare(a.name, b.name, StringComparison.Ordinal));
+        }
+
+        private void Start()
+        {
+            // Rotate all puzzles randomly. Can be rotated 0 times or 3 times (second parameter of range is exclusive)
+            foreach (var puzzle in _puzzles) puzzle.Rotate(Random.Range(0, 4));
+        }
+        
+        public void PuzzleWasRotated()
+        {
+            bool solved = true;
+            // Check if all puzzles are upright
+            foreach (var rotatingPuzzle in _puzzles)
+                if (!rotatingPuzzle.IsUpright()) solved = false;
+            if (solved)
+                Debug.Log("Puzzle solved!");
+        }
+    }
+}
