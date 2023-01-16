4cf291d0db3f980e27c90cfcadfb52cb51aa4caf
Alejo Ramirez Gismondi <alejo.ramirezgismondi@ing.austral.edu.ar>
2023-01-16 01:09:58 -0300

    (Chore): Finished puzzle 2. Missing reward

---
+++ Assets/Scripts/Puzzle2/SwitchPuzzle.cs
@@ -0,0 +1,51 @@
+using Inventory.Scripts;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Puzzle2
+{
+    public class SwitchPuzzle : Interactable
+    {
+        [SerializeField] private SwitchPuzzle[] connectedPuzzles;
+        [SerializeField] private Sprite activatedSprite;
+        [SerializeField] private Sprite deactivatedSprite;
+        [SerializeField] private bool activated;
+        private SwitchPuzzleManager _manager;
+        private SpriteRenderer _spriteRenderer;
+
+        private void Awake()
+        {
+            _spriteRenderer = gameObject.GetComponentInChildren<SpriteRenderer>();
+            _manager = GetComponentInParent<SwitchPuzzleManager>();
+        }
+
+        void Start()
+        {
+            _spriteRenderer.sprite = activated ? activatedSprite : deactivatedSprite;
+        }
+
+        private void Flip()
+        {
+            activated = !activated;
+            _spriteRenderer.sprite = activated ? activatedSprite : deactivatedSprite;
+            _manager.SwitchWasFlipped();
+        }
+        
+        public bool IsActivated()
+        {
+            return activated;
+        }
+
+        public void Activate()
+        {
+            activated = true;
+            _spriteRenderer.sprite = activatedSprite;
+        }
+        
+        public override void Interact(InventoryComponent inventoryComponent)
+        {
+            Flip();
+            foreach (var puzzle in connectedPuzzles) puzzle.Flip();
+        }
+    }
+}
---
+++ Assets/Scripts/Puzzle2/SwitchPuzzleManager.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+namespace Puzzle2
+{
+    public class SwitchPuzzleManager : MonoBehaviour
+    {
+        private SwitchPuzzle[] _puzzles;
+
+        private void Awake()
+        {
+            _puzzles = GetComponentsInChildren<SwitchPuzzle>();
+            // Sort puzzles by name
+            Array.Sort(_puzzles, (a, b) => String.Compare(a.name, b.name, StringComparison.Ordinal));
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            for (var i = 0; i < _puzzles.Length; i++) if (i % 2 == 0) _puzzles[i].Activate();
+        }
+
+        public void SwitchWasFlipped()
+        {
+            var isSolved = true;
+            foreach (var puzzle in _puzzles) if (!puzzle.IsActivated()) isSolved = false;
+            if (isSolved) Debug.Log("Puzzle solved!");
+        }
+    }
+}
