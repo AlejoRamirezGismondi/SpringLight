f2e8dcf926a4db2d3d079466d1c528db1201b140
Alejo Ramirez Gismondi <alejo.ramirezgismondi@ing.austral.edu.ar>
2023-01-18 00:34:28 -0300

    (feat): Save & Load player position

--- Assets/Scripts/DataPersistence/Data/GameData.cs
+++ Assets/Scripts/DataPersistence/Data/GameData.cs
@@ -7,11 +7,13 @@ namespace DataPersistence.Data
     [Serializable]
     public class GameData
     {
+        public float[] playerPosition;
         public int coins;
         public List<CropTileData> CropTileDataList;
 
         public GameData()
         {
+            playerPosition = new float[3];
             coins = 0;
             CropTileDataList = new List<CropTileData>();
         }
---
+++ Assets/Scripts/DataPersistence/PlayerPersistence.cs
@@ -0,0 +1,24 @@
+﻿using DataPersistence.Data;
+using UnityEngine;
+
+namespace DataPersistence
+{
+    public class PlayerPersistence : MonoBehaviour, IDataPersistence
+    {
+        public void LoadData(GameData data)
+        {
+            transform.position = new Vector3(data.playerPosition[0], data.playerPosition[1], data.playerPosition[2]);
+        }
+
+        public void SaveData(GameData data)
+        {
+            var position = transform.position;
+            data.playerPosition = new[]
+            {
+                position.x,
+                position.y,
+                position.z,
+            };
+        }
+    }
+}
